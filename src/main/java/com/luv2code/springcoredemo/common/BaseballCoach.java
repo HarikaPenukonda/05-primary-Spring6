1a8e7ef58149922c8c9748850bae11f68d0c8ae5
harika penukonda <penukondaharika@gmail.com>
2023-05-23 00:38:12 -0500

    Adding Primary annotation

--- src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -1,7 +1,8 @@
 package com.luv2code.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-
+@Primary
 @Component
 public class BaseballCoach implements Coach{
     @Override
